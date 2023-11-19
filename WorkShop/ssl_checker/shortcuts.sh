685124581a3490baa64800e3c2c96be86061cd91
Anthony Bagrianov <rudiemeant@gmail.com>
2023-11-20 00:08:50 +0400

    Update via WSL

---
+++ WorkShop/ssl_checker/shortcuts.sh
@@ -0,0 +1,6 @@
+﻿# get full certificate chain
+openssl s_client -showcerts -connect www.google.com:443 </dev/null
+
+# Get supported ciphers
+openssl ciphers 'ALL:eNULL'
+nmap -sV --script ssl-enum-ciphers -p 443 www.google.co
